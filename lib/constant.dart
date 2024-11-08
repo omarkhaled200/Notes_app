6f724f2cffeedd8262e6eb68a6914d3b8dd295aa
omarkhaled200 <omarkhaled.6403@gmail.com>
2024-11-08 16:03:48 +0200

    init Hive and open notes box

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kprimarycolor= Color(0xff54EED9);
+const kprimarycolor = Color(0xff54EED9);
+const kNotesBox = "note_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Views/edit_note_view_body.dart';
 import 'package:notes_app/Views/notes_view.dart';
+import 'package:notes_app/constant.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      brightness: Brightness.dark,
-      fontFamily: 'Poppins',
-    ),
-    routes: {
-      NotesView.id:(context)=>NotesView(),
-      EditNoteViewBody.id:(context)=>EditNoteViewBody(),
-    },
-    initialRoute: NotesView.id,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
+      ),
+      routes: {
+        NotesView.id: (context) => const NotesView(),
+        EditNoteViewBody.id: (context) => const EditNoteViewBody(),
+      },
+      initialRoute: NotesView.id,
     );
   }
 }
